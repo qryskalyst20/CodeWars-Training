17a2f5649be01acfc096cd12e7bb7893def5c9a4
Qagura <quddusnuriman@gmail.com>
2023-02-11 13:57:38 +0800

    initial commit

---
+++ NarcissisticNumbers/NarcissisticNumbers.java
@@ -0,0 +1,14 @@
+package NarcissisticNumbers;
+import java.util.*;
+
+public class NarcissisticNumbers {
+
+    public static boolean isNarcissistic(int number){
+        
+        return false;
+    }
+    public static void main(String[] args){
+        
+
+    }
+}
